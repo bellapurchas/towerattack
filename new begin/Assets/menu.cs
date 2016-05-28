ab82c11cd312c32a43579798783e7d1589bdd70e
Isabella Purchas <bella@TPF.local>
2016-05-28 10:47:44 +1000

    adding Kevins new beginning

---
+++ new begin/Assets/menu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour {
+
+	public AudioClip buttonPress;
+	public AudioSource audioSource;
+
+
+
+	public void star(){
+		
+	
+		SceneManager.LoadScene ("");
+	}
+
+	public void selectLevel(){
+		SceneManager.LoadScene ("selectLevel");
+	}
+	public void level1(){
+		SceneManager.LoadScene ("");
+	}
+	public void level2(){
+		SceneManager.LoadScene ("");
+	}
+	public void level3(){
+		SceneManager.LoadScene ("");
+	}
+	public void level4(){
+		SceneManager.LoadScene ("");
+	}
+	public void level5(){
+		SceneManager.LoadScene ("");
+	}
+	public void playClip(AudioClip x){
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.PlayOneShot (x);
+	}
+}
---
+++ new begin/Assets/story.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class story : MonoBehaviour {
+
+	float[] timeArray = new float[]{2,4,6,8,10};
+	int index;
+
+
+	public GameObject Canvas;
+	public GameObject enemy;
+	public GameObject teleport_active;
+
+	// Use this for initialization
+	void Start () {
+		
+		enemy.SetActive (false);
+		teleport_active.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (index < timeArray.Length && Time.realtimeSinceStartup >= timeArray [index]) {
+			OnTime (index);
+
+			index++;
+		}
+	}
+
+	void OnTime(int index)
+	{
+		Debug.Log (index);
+		switch (index) {
+
+		case 0:
+			{
+				teleport_active.SetActive(true);
+			}
+			break;
+		case 1:
+			{
+				enemy.SetActive(true);
+			}
+			break;
+		case 2:
+			{
+				Canvas.SetActive (true);
+			}
+			break;
+		}
+
+
+	}
+
+
+
+}
