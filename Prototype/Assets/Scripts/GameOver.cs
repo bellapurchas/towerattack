6cb998deaf3882e7932c28642f01056ac698f2fc
Sam Matthews <sam.matthews.77@gmail.com>
2016-06-03 13:50:25 +1000

    Level transitions

--- Prototype/Assets/Scripts/GameOver.cs
+++ Prototype/Assets/Scripts/GameOver.cs
@@ -3,13 +3,20 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour {
-	void nextLevel(){
-	int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; 
-		if (currentSceneIndex < SceneManager.sceneCount - 1) {
-			SceneManager.LoadScene (currentSceneIndex + 1);
-		}
+
+
+
+
+
+	void Start () {
+
 	}
-	// Use this for initialization
+	void RestartLevel (){
 
+		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; 
 
+		SceneManager.LoadScene(currentSceneIndex + 1);
+
+	}
 }
+
